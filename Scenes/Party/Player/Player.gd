7c1634aaba95af3e02778c76e24ba91b0b72b607
Mai Li <maili.ra.goodman@gmail.com>
2021-09-28 19:05:25 -0400

    move bugfix

--- Scenes/Party/Player/Player.gd
+++ Scenes/Party/Player/Player.gd
@@ -9,7 +9,7 @@ var inputs = {
 
 onready var ray = $RayCast2D 
 onready var tween = $Tween 
-export var speed = 3.15 
+export var speed = 3.15
 var last_dir
 var want_to_move = false
 
@@ -29,10 +29,7 @@ func _process(delta):
 func _input(event):
 	for dir in inputs.keys():
 		if event.is_action_pressed(dir, true):
-			if last_dir != dir:
-				#somewhat concerned that the tween instructions here effect ALL tweens
-				print("change dir")
-				want_to_move = true
+			
 			if tween.is_active() == false:
 				move(inputs[dir])
 			else: 
@@ -42,12 +39,17 @@ func _input(event):
 				#don't keep moving unless the character is on the outer bounds of the tile
 				if 75 < x_pos and x_pos < 25 and 75 < y_pos and y_pos < 25 :
 					want_to_move = true
-			last_dir = dir
+				if last_dir != dir:
+				#somewhat concerned that the tween instructions here effect ALL tweens
+					want_to_move = true
+			print(last_dir)
+			if dir != null:
+				last_dir = dir
 
 func move_tween(dir):
 	tween.interpolate_property(self, "position",
 		position, position + dir * tile_size,
-		1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		1.0/speed, Tween.TRANS_QUAD)
 	tween.start()
 
 func move(dir):
