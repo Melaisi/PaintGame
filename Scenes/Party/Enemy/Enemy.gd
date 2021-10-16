eac1d519589c6afc79530fac71c41287f1811df3
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-10-16 12:21:40 -0400

    Update enemy animation based on encounter result

--- Scenes/Party/Enemy/Enemy.gd
+++ Scenes/Party/Enemy/Enemy.gd
@@ -7,19 +7,23 @@ extends Area2D
 
 var current_state = "none"
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
+	print("ENEMY --- READY")
 	update_state() 
+	
+
 
 func update_state():
+	
 	# update state of player from global script 
 	current_state = get_node("/root/Global").enemy_state 
+	print ("Current State is ", current_state)
 	
-	# Make sure both state not visible 
-	get_node("notHappy").visible = false
-	get_node("Happy").visible = false
-	
-	# toogle visibility based on global state
+	# toogle animation based on global state
 	if current_state == "HAPPY":
-		get_node("Happy").visible = true
+		print("Current state = Happy")
+		get_node("AnimatedSprite").play("HAPPY")
 	else:
-		get_node("notHappy").visible = true
+		print ("Current stae != happy")
+		get_node("AnimatedSprite").play("NOT_HAPPY")
