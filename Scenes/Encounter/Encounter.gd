e021aba30776264ef7940ba23dd4026bc44903a9
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-10-16 12:05:32 -0400

    Testing changing of global enemy state based on encounter result -- works

---
+++ Scenes/Encounter/Encounter.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#For testing only 
+	won()
+	#lost()
+
+func won():
+	# Yay! change to enemy global variable to happy 
+	get_node("/root/Global").set_enemy_happy() 
+	# Go back to previous scene # TODO 
+	get_node("/root/Global").setScene("res://Scenes/Main/MainLevel.tscn") 
+	
