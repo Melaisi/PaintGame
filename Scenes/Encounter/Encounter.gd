6e6fc16764e7b43737f044eb8cdae7807f4ab627
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-10-24 10:08:01 -0400

    Adding player without script to encounter scene

--- Scenes/Encounter/Encounter.gd
+++ Scenes/Encounter/Encounter.gd
@@ -1,18 +1,14 @@
-extends Sprite
-
-
-
-
+extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#For testing only 
-	won()
+	#won()
 	#lost()
 	pass
 
 func won():
 	# Yay! change to enemy global variable to happy 
-	get_node("/root/Global").set_enemy_happy() 
+	#get_node("/root/Global").set_enemy_happy() 
 	# Go back to previous scene # TODO 
-	get_node("/root/Global").setScene("res://Scenes/Main/MainLevel.tscn") 
-	
+	#get_node("/root/Global").setScene("res://Scenes/Main/MainLevel.tscn") 
+	pass
